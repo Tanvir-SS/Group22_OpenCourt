f33782030b5e4c9ec388684ac5f7dc6199df4c9a
Rohin Aulakh <rohinaulakh@gmail.com>
2025-11-04 23:02:49 -0800

    map controls added and user location but its slow rn

--- app/src/main/java/com/example/group22_opencourt/ui/main/MapFragment.kt
+++ app/src/main/java/com/example/group22_opencourt/ui/main/MapFragment.kt
@@ -1,24 +1,30 @@
 package com.example.group22_opencourt.ui.main
 
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.group22_opencourt.MainActivity
+import com.example.group22_opencourt.R
 
 import com.example.group22_opencourt.databinding.FragmentMapBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlin.text.replace
 
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var binding: FragmentMapBinding
-
     private lateinit var map: GoogleMap
+    private var mapCentered = false
 
 
     override fun onCreateView(
@@ -30,22 +36,46 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val mapFragment = childFragmentManager.findFragmentById(binding.map.id) as SupportMapFragment
+        var mapFragment = childFragmentManager.findFragmentById(R.id.map_container) as? SupportMapFragment
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance()
+            childFragmentManager.beginTransaction()
+                .replace(R.id.map_container, mapFragment)
+                .commit()
+        }
         mapFragment.getMapAsync(this)
-
-
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        map.mapType = GoogleMap.MAP_TYPE_NORMAL
-        val lat = 49.27984399307886
-        val long = -122.92159771848385
-        val latlong = LatLng(lat, long)
-        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latlong, 14f)
-        map.animateCamera(cameraUpdate)
-
+        val options = GoogleMapOptions()
+        options.mapType(GoogleMap.MAP_TYPE_NORMAL)
+        map.uiSettings.isZoomControlsEnabled = true
+        map.uiSettings.isCompassEnabled = true
+        // get user location from MainActivity and update map
+        val location = (activity as? MainActivity)?.currentLocation
+        if (location != null) {
+            updateUserLocation(location)
+        } else {
+            // use sfu as default location
+            val sfuLatLng = Location("").apply {
+                latitude = 49.27984399307886
+                longitude = -122.92159771848385
+            }
+            updateUserLocation(sfuLatLng)
+        }
     }
 
+    fun updateUserLocation(location: Location) {
+        val latLng = LatLng(location.latitude, location.longitude)
+        map.clear()
+        map.addMarker(MarkerOptions().position(latLng).title("You are here"))
+        // center map on user location
+        if (!mapCentered) {
+            val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16f)
+            map.animateCamera(cameraUpdate)
+            mapCentered = true
+        }
+    }
 
 }
